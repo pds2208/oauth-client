20fd4dc61e6479de8d57d96c5f79871d5d1debe4
Paul Soule <paul@souletech.com>
2018-05-09 10:00:12 +0100

    Initial version supporting CA API Gateway

--- src/main/scala/uk/gov/ons/OAuthClient.scala
+++ src/main/scala/uk/gov/ons/OAuthClient.scala
@@ -1,15 +1,15 @@
 package uk.gov.ons
 
+import javax.net.ssl.SSLSession
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.ComponentScan
-
 import uk.gov.ons.configuration.OAuthConfiguration
 import uk.gov.ons.controller.MessageController
 
 object OAuthClient {
-  def main(args: Array[String]) : Unit = {
-    SpringApplication.run(classOf[OAuthClient], args :_ *)
+  def main(args: Array[String]): Unit = {
+    SpringApplication.run(classOf[OAuthClient], args: _ *)
   }
 }
 
@@ -18,5 +18,31 @@ object OAuthClient {
   classOf[MessageController],
   classOf[OAuthConfiguration])
 )
-class OAuthClient {}
+class OAuthClient {
+
+  /** For localhost testing.
+    *
+    * This code is needed together with a certificate store containing
+    * a certificate for localhost.
+    *
+    * Use the InstallCert application to generate a valid jssecacerts file.
+    * Then copy the file to a location of your choice and add the following
+    * command line option:
+    *
+    * -Djavax.net.ssl.trustStore=<location of your choice>jssecacerts
+    *
+    * */
+
+  javax.net.ssl.HttpsURLConnection.setDefaultHostnameVerifier(
+    new javax.net.ssl.HostnameVerifier() {
+
+      override def verify(hostname: String, sslSession: SSLSession): Boolean = {
+        if (hostname == "localhost") {
+          return true
+        }
+        false
+      }
+    })
+
+}
 
